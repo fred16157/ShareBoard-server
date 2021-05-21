55165b023cbdbe472cb5870bc695ede52b37993a
강현구 <fred16157@gmail.com>
2021-05-21 10:07:03 +0900

    이미지 지원(베타)

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const io = require('socket.io')(server);
 const consola = require('consola');
 
 io.on('connection', (socket) => {
-    socket.on('copy', (data) => {
-        consola.info('copy called - ' + data);
-        socket.broadcast.emit('paste', data);
+    socket.on('copy-text', (data) => {
+        consola.info('Text copy called - ' + data);
+        socket.broadcast.emit('paste-text', data);
+        consola.success('Broadcasted text ' + data);
+    });
+
+    socket.on('copy-image', (data) => {
+        consola.info('Image copy called');
+        socket.broadcast.emit('paste-image', data);
+        consola.success('Broadcasted image');
     });
 });
 
